01f804d9aa4d9b6c10c8936b849b9a88d197b450
ilayKeidar <ilykeidar10@gmail.com>
2025-01-03 13:17:04 +0200

    Removed unnecessary files

--- test.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import sniff, DNS
-
-def packet_callback(packet):
-    if packet.haslayer(DNS):  # Check if the packet contains a DNS layer
-        print(packet.summary())  # Print a summary of the DNS packet
-
-# Start sniffing packets
-print("Sniffing DNS packets... Press Ctrl+C to stop.")
-sniff(filter="udp port 53", prn=packet_callback, store=False)
